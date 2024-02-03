11e617d9282459d400e273f41089c61e5ac97046
Keyan <keyanjavanshir@live.dk>
2024-02-03 13:43:20 +0100

    Code is now functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+// JOB ARRAY TO RENDER
+let companyList = []
+
 // IDs from HTML
 const inputForm = document.getElementById("input-form")
 const addSiteBtn = document.getElementById("btn-site")
@@ -6,14 +9,48 @@ const deleteBtn = document.getElementById("btn-delete")
 const logSites = document.getElementById("log")
 
 // Accessing localStorage
-const jobsFromLocalStorage = JSON.parse( localStorage.getItem("jobList") )
-
-// JOB ARRAY TO RENDER
-let jobList = []
+const companiesFromLocalStorage = JSON.parse( localStorage.getItem("myCompanies") )
 
 
-// TAB TEST VARIABLE
-let tab = 
-    { 
-        url: "www.facebook.com"
+if (companiesFromLocalStorage) {
+    companyList = companiesFromLocalStorage
+    render(companyList)
+}
+
+
+function render(companies) {
+    let listItems = ""
+    for (let i = 0; i < companies.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${companies[i]}'>
+                    ${companies[i]}
+                </a>
+            </li>
+        `
     }
+    logSites.innerHTML = listItems;
+}
+
+addTabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        companyList.push(tabs[0].url)
+        localStorage.setItem("myCompanies", JSON.stringify(companyList) )
+        render(companyList)
+    })
+})
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    companyList = []
+    render(companyList)
+})
+
+
+addSiteBtn.addEventListener("click", function() {
+    companyList.push(inputForm.value)
+    inputForm.value = ""
+    localStorage.setItem("myCompanies", JSON.stringify(companyList) )
+    render(companyList)
+})
