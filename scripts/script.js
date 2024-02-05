7dc876d616a9c69bcf72d199445df7a8aaed87b8
Keyan <keyanjavanshir@live.dk>
2024-02-05 16:44:38 +0100

    Added delete last element button

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ let companyList = [];
 const inputForm = document.getElementById("input-form")
 const addSiteBtn = document.getElementById("btn-site")
 const addTabBtn = document.getElementById("btn-tab")
+const deleteLast = document.getElementById("btn-deleteLast")
 const deleteBtn = document.getElementById("btn-delete")
 const logSites = document.getElementById("log")
 const printBtn = document.getElementById("btn-print")
@@ -46,14 +47,21 @@ addTabBtn.addEventListener("click", function(){
 })
 
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteLast.addEventListener("dblclick", () => {
+    companyList.pop()
+    localStorage.setItem("myCompanies", JSON.stringify(companyList) )
+    render(companyList)
+})
+
+
+deleteBtn.addEventListener("dblclick", () => {
     localStorage.clear()
     companyList = []
     render(companyList)
 })
 
 
-addSiteBtn.addEventListener("click", function() {
+addSiteBtn.addEventListener("click", () => {
     if(inputForm.value === "" || inputForm.value === "undefined") {
         console.error("Please input a valid URL.")
     } else {
